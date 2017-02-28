7f596d5eb189702e6958e63510e03590c4def840
lin ting-yang <toy50107@gmail.com>
2017-03-01 00:11:11 +0800

    add user notification

--- reminder/AddEventController.swift
+++ reminder/AddEventController.swift
@@ -4,26 +4,34 @@
 //
 
 import UIKit
+import UserNotifications
 
 class AddEventController: ReminderDataViewController {
     
     @IBOutlet var myTitle: UITextField!
     @IBOutlet var myContent: UITextView!
+    
+    var eventTitle = ""
+    var eventContent = ""
     var dateComponents = [String]()
     var alarmData: Date?
-    
+    let requestIdentifier = "SampleRequest"
     // 取得螢幕的尺寸
-    let fullScreenSize =
-        UIScreen.main.bounds.size
+    let fullScreenSize = UIScreen.main.bounds.size
     
     //MARK: 儲存資料
     @IBAction func save(_ sender: Any) {
-        var eventTitle = myTitle.text
-        var eventContent = myContent.text
+        eventTitle = myTitle.text!
+        eventContent = myContent.text
         if eventTitle == "" { eventTitle = "New Event" }
         if eventContent == "" { eventContent = "No Content" }
         //寫入資料
-        writePlist(title: eventTitle!, content: eventContent!, date: dateComponents)
+        writePlist(title: eventTitle, content: eventContent, date: dateComponents)
+        
+        //觸發通知
+        if dateComponents.count != 0{
+            triggerNotification()
+        }
         
         self.performSegue(withIdentifier: "unwindToMenu", sender: self)
     }
@@ -37,6 +45,12 @@ class AddEventController: ReminderDataViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "unwindToMenu" {
+            
+        }
     }
     
     //MARK: Back to Event
@@ -53,7 +67,7 @@ class AddEventController: ReminderDataViewController {
         dateComponents.append(dateFormatter.string(from: date))
         dateFormatter.dateFormat = "dd"
         dateComponents.append(dateFormatter.string(from: date))
-        dateFormatter.dateFormat = "h"
+        dateFormatter.dateFormat = "H"
         dateComponents.append(dateFormatter.string(from: date))
         dateFormatter.dateFormat = "mm"
         dateComponents.append(dateFormatter.string(from: date))
@@ -64,4 +78,25 @@ class AddEventController: ReminderDataViewController {
         myContent.layer.cornerRadius = 10
     }
     
+    //MARK: 觸發通知
+    func triggerNotification(){
+        let content = UNMutableNotificationContent()
+        content.title = eventTitle
+        content.body = eventContent
+        content.sound = UNNotificationSound.default()
+        
+        var date = DateComponents()
+        date.month = Int(dateComponents[0])
+        date.day = Int(dateComponents[1])
+        date.hour = Int(dateComponents[2])
+        date.minute = Int(dateComponents[3])
+        
+        let trigger = UNCalendarNotificationTrigger.init(dateMatching: date, repeats: false)
+        //測試用trigger
+        //let trigger = UNTimeIntervalNotificationTrigger.init(timeInterval: 5.0, repeats: false)
+        let request = UNNotificationRequest(identifier: date.description, content: content, trigger: trigger)
+        
+        UNUserNotificationCenter.current().add(request, withCompletionHandler: nil)
+    }
 }
+
--- reminder/AppDelegate.swift
+++ reminder/AppDelegate.swift
@@ -7,7 +7,7 @@ import UIKit
 import UserNotifications
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterDelegate{
 
     var window: UIWindow?
 
@@ -20,8 +20,18 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         center.requestAuthorization(options: [.alert, .sound]) { (granted, error) in
             // Enable or disable features based on authorization.
         }
+        UNUserNotificationCenter.current().delegate = self
         
         return true
+    }
+    
+    //MARK: User Notification
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+    }
+    
+    //This is key callback to present notification while the app is in foreground
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        completionHandler( [.alert,.sound,.badge])
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
@@ -45,7 +55,4 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
-
-
 }
-
