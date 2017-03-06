880d3d8fcd4a986a7536890dcc103777854fce62
lin ting-yang <toy50107@gmail.com>
2017-03-06 16:18:04 +0800

    no message

--- reminder/CustomTableViewCell.swift
+++ reminder/CustomTableViewCell.swift
@@ -9,11 +9,12 @@ class CustomTableViewCell: UITableViewCell {
     //MARK: UI
     @IBOutlet var eventTitle: UILabel!
     @IBOutlet var eventContent: UILabel!
-    @IBOutlet var imgView: UIImageView!
+    @IBOutlet var imgView: UIImageView!    
     
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
         
         // Configure the view for the selected state
+       
     }
 }
